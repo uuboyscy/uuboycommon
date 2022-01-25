640b4e4dfa3865e685b74b8a2ae070ea9d067faf
uuboyscy <u.shi@lndata.com>
2022-01-26 00:10:54 +0800

    Yaml config loader added

---
+++ src/main/java/uuboy/scy/Test.java
@@ -0,0 +1,14 @@
+package uuboy.scy;
+
+
+/**
+ * Project: uuboycommon
+ * Package: uuboy.scy
+ * Test.java
+ * Description:
+ * User: uuboyscy
+ * Created Date: 1/25/22
+ * Version: 0.0.1
+ */
+public class Test {
+}
---
+++ src/main/java/uuboy/scy/common/util/ConfigLoader.java
@@ -0,0 +1,34 @@
+package uuboy.scy.common.util;
+
+/**
+ * Project: uuboycommon
+ * Package: uuboy.scy.common.util
+ * ConfigLoader.java
+ * Description:
+ * User: uuboyscy
+ * Created Date: 1/25/22
+ * Version: 0.0.1
+ */
+
+import org.yaml.snakeyaml.Yaml;
+import java.io.InputStream;
+import java.util.Map;
+
+public class ConfigLoader {
+
+    public ConfigLoader() {
+    }
+
+    public static Object loadConfig(String config) {
+        InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream(config);
+        Yaml yaml = new Yaml();
+        return yaml.load(input);
+    }
+
+    public static void main(String[] args) {
+        String filename = "test.yaml";
+        Map<String, Map<String, String>> baseConfig = (Map<String, Map<String, String>>) ConfigLoader.loadConfig(filename);
+        System.out.println(baseConfig.get("test1").get("a"));
+        System.out.println(baseConfig.get("test2").get("cc"));
+    }
+}
