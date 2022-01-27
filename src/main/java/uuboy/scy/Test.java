caed5f7fd425cb7ee2db72bf6a958d0dc5ddbb5a
uuboyscy <u.shi@lndata.com>
2022-01-28 00:25:32 +0800

    Refactored pom.xml

---
+++ src/main/java/uuboy/scy/Test.java
@@ -0,0 +1,37 @@
+package uuboy.scy;
+
+import uuboy.scy.common.util.*;
+import uuboy.scy.common.api.*;
+
+import java.text.ParseException;
+import java.util.Map;
+
+/**
+ * Project: uuboycommon
+ * Package: uuboy.scy
+ * Test.java
+ * Description:
+ * User: uuboyscy
+ * Created Date: 1/25/22
+ * Version: 0.0.1
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        ApiHelper apiHelper = new ApiHelper();
+        String filename = "test.yaml";
+        Map<String, Map<String, String>> baseConfig = (Map<String, Map<String, String>>) ConfigLoader.loadConfig(filename);
+        System.out.println(baseConfig.get("test1").get("a"));
+        System.out.println(baseConfig.get("test2").get("cc"));
+        try {
+            ApiHelper.getObj("https://google.com",null, null, null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            DateUtils.main(null);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
