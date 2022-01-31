f380b6466ee37e3db84e42013ad1f3f715170da6
uuboyscy <u.shi@lndata.com>
2022-01-31 18:40:41 +0800

    S3 tools added

--- src/main/java/uuboy/scy/common/file/s3Client/BucketOperation.java
+++ src/main/java/uuboy/scy/common/file/s3Client/BucketOperation.java
@@ -1,5 +1,17 @@
 package uuboy.scy.common.file.s3Client;
 
+//import com.amazonaws.regions.Regions;
+import com.amazonaws.util.StringUtils;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Project: uuboycommon
  * Package: uuboy.scy.common.file.s3Client
@@ -9,5 +21,119 @@ package uuboy.scy.common.file.s3Client;
  * Created Date: 1/31/22
  * Version: 0.0.1
  */
+
 public class BucketOperation {
+
+    /** Get a list of bucket objects */
+    public List<Bucket> getBucketList(){
+        AmazonS3 conn = (new GetCredential()).conn;
+        List<Bucket> buckets = conn.listBuckets();
+        return buckets;
+    }
+
+    /** Get one bucket object */
+    public Bucket getBucket(String bucketName){
+        AmazonS3 conn = (new GetCredential()).conn;
+        List<Bucket> buckets = conn.listBuckets();
+        Bucket outputBucket = null;
+        for (Bucket bucket : buckets){
+            if (bucket.getName().equals(bucketName)){
+                outputBucket = bucket;
+                break;
+            }
+        }
+        return outputBucket;
+    }
+
+    /** List all buckets */
+    public void listAllBuckets(){
+        AmazonS3 conn = (new GetCredential()).conn;
+        List<Bucket> buckets = conn.listBuckets();
+        for (Bucket bucket : buckets){
+            System.out.println(bucket.getName() + "\t" +
+                    StringUtils.fromDate(bucket.getCreationDate()));
+        }
+    }
+
+    /** Create new bucket */
+    public void createBucket(String bucketName){
+        AmazonS3 conn = (new GetCredential()).conn;
+        Bucket bucket = conn.createBucket(bucketName);
+    }
+
+    /** Delete bucket */
+    public void deleteBucket(String bucketName){
+    }
+
+    /** Get a list of content objects in certain bucket */
+    public List<S3ObjectSummary> getBucketContents(String bucketName){
+        List<S3ObjectSummary> contentsList = new ArrayList<S3ObjectSummary>();
+        AmazonS3 conn = (new GetCredential()).conn;
+        Bucket bucket = getBucket(bucketName);
+        ObjectListing objects = conn.listObjects(bucket.getName());
+        do {
+            for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
+                contentsList.add(objectSummary);
+            }
+            objects = conn.listNextBatchOfObjects(objects);
+        } while (objects.isTruncated());
+        return contentsList;
+    }
+
+    /** List all contents in certain bucket
+     * come with size and modified date */
+    public void listBucketContents(String bucketName){
+        AmazonS3 conn = (new GetCredential()).conn;
+        Bucket bucket = getBucket(bucketName);
+        ObjectListing objects = conn.listObjects(bucket.getName());
+        do {
+            for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
+                System.out.println(objectSummary.getKey() + "\t" +
+                        objectSummary.getSize() + " Bytes" + "\t" +
+                        StringUtils.fromDate(objectSummary.getLastModified()));
+            }
+            objects = conn.listNextBatchOfObjects(objects);
+        } while (objects.isTruncated());
+    }
+
+    /** Download one content object from certain bucket */
+    public void downloadBucketContent(String bucketName, String contentName, String downloadPath){
+        AmazonS3 conn = (new GetCredential()).conn;
+        Bucket bucket = getBucket(bucketName);
+        conn.getObject(
+                new GetObjectRequest(bucket.getName(), contentName),
+                new File(downloadPath)
+        );
+    }
+
+    /** Upload file to certain bucket */
+    public void uploadFile(String bucketName, String localFilePath, String s3Path){
+        AmazonS3 conn = (new GetCredential()).conn;
+        Bucket bucket = getBucket(bucketName);
+        conn.putObject(bucket.getName(), s3Path, new File(localFilePath));
+    }
+
+    /** Delete one content object in certain bucket */
+    public void deleteBucketContent(String bucketName, String contentName){
+        AmazonS3 conn = (new GetCredential()).conn;
+        conn.deleteObject(bucketName, contentName);
+    }
+
+
+    public static void main(String[] args){
+
+        BucketOperation bopt = new BucketOperation();
+        bopt.listBucketContents("some-bucket-name");
+
+//        Bucket bucket = (new BucketOperation()).getBucket("some-bucket-name");
+//        System.out.println(bucket.toString());
+
+//        BucketOperation b = new BucketOperation();
+//        b.listAllBuckets();
+
+//        (new BucketOperation())
+//                .downloadBucketContent("some-bucket-name", "test.seq", "/Users/uuboy.scy/testS3DownloadFile.seq");
+
+    }
+
 }
--- src/main/java/uuboy/scy/common/file/s3Client/GetCredential.java
+++ src/main/java/uuboy/scy/common/file/s3Client/GetCredential.java
@@ -1,5 +1,21 @@
 package uuboy.scy.common.file.s3Client;
 
+import com.amazonaws.regions.Regions;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.Grant;
+import com.amazonaws.util.StringUtils;
+
+import java.util.List;
+
 /**
  * Project: uuboycommon
  * Package: uuboy.scy.common.file.s3Client
@@ -9,5 +25,26 @@ package uuboy.scy.common.file.s3Client;
  * Created Date: 1/31/22
  * Version: 0.0.1
  */
+
 public class GetCredential {
+
+    public static String access = "access"; //ex: AKIA3PGEPUAAAAAABBBB
+    public static String secret = "secret"; //ex: W3pIE45y64DAjeRXyBUQAAAAAAAAAABBBBBBBBBB
+    public static Regions regions = Regions.AP_NORTHEAST_1;
+
+    // Create connection
+    AWSCredentials creds = new BasicAWSCredentials(access, secret);
+    public AmazonS3 conn = new AmazonS3Client(creds);
+
+    public static void main(String[] args){
+
+        // Create connection
+        AmazonS3 conn = (new GetCredential()).conn;
+
+        List<Bucket> buckets = conn.listBuckets();
+        for (Bucket bucket : buckets) {
+            System.out.println(bucket.getName() + "\t" +
+                    StringUtils.fromDate(bucket.getCreationDate()));
+        }
+    }
 }
