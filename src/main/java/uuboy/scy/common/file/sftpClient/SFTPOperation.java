f760d18dea5299f16b8b08b9c354616b8ed2e37a
uuboyscy <u.shi@lndata.com>
2022-02-01 20:24:45 +0800

    Added SFTP dependencies

---
+++ src/main/java/uuboy/scy/common/file/sftpClient/SFTPOperation.java
@@ -0,0 +1,13 @@
+package uuboy.scy.common.file.sftpClient;
+
+/**
+ * Project: uuboycommon
+ * Package: uuboy.scy.common.file.sftpClient
+ * SFTPOperation.java
+ * Description:
+ * User: uuboyscy
+ * Created Date: 2/1/22
+ * Version: 0.0.1
+ */
+public class SFTPOperation {
+}
